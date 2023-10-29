9470a2282ed7a34851154cdff538c5cdb1150713
andrea <andrea.zollini@proton.me>
2023-10-29 13:18:43 +0100

    dettagli piatto

--- lib/widgets/meal_item_trait.dart
+++ lib/widgets/meal_item_trait.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class MealItemTrait extends StatelessWidget {
-  const MealItemTrait({super.key});
+  const MealItemTrait({
+    super.key,
+    required this.icon,
+    required this.label,
+  });
 
   final IconData icon;
   final String label;
@@ -9,9 +13,20 @@ class MealItemTrait extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(children: [
-      Icon(icon),
-      const SizedBox(),
-      Text(data),
+      Icon(
+        icon,
+        size: 17,
+        color: Colors.white,
+      ),
+      const SizedBox(
+        width: 6,
+      ),
+      Text(
+        label,
+        style: const TextStyle(
+          color: Colors.white,
+        ),
+      ),
     ]);
   }
 }
